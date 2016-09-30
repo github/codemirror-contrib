afa53c97a488f50452b5998f6a75769df4bdaa87
larshp <larshp@hotmail.com>
2016-09-30 11:02:54 +0000

    cleanup

--- src/mode/meta.js
+++ src/mode/meta.js
@@ -1,4 +1,5 @@
 import CodeMirror from "codemirror/lib/codemirror"
+import * as ABAP from "./abap/abap.js";
 
 CodeMirror.modeInfo = CodeMirror.modeInfo.concat([
   {name: "ABAP", mime: "text/abap", mode: "abap", ext: ["abap"]},
