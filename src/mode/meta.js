8ce126a685d32a65587ad8339d763d5f8d4ce611
Joshua Peek <josh@joshpeek.com>
2016-10-05 10:13:24 -0700

    Meta should not import modes

--- src/mode/meta.js
+++ src/mode/meta.js
@@ -1,5 +1,4 @@
 import CodeMirror from "codemirror/lib/codemirror"
-import "./abap/abap"
 
 CodeMirror.modeInfo = CodeMirror.modeInfo.concat([
   {name: "ABAP", mime: "text/abap", mode: "abap", ext: ["abap"]}
