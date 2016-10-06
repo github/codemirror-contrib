2a808bcb16b609279cac4250cfc450f7555c2501
Joshua Peek <josh@joshpeek.com>
2016-10-06 10:53:41 -0700

    Hush rollup globals errors

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,10 @@ export default {
     'codemirror/lib/codemirror',
     'codemirror/lib/mode/meta'
   ],
+  globals: {
+    'codemirror/lib/codemirror': 'CodeMirror',
+    'codemirror/lib/mode/meta': 'undefined'
+  },
   paths: {
     'codemirror': 'codemirror/lib/codemirror'
   },
